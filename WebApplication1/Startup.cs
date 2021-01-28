ba7fdc9cebd1cc4a70c9a0ae346dcf684602f006
Sebastian <sebastian@squidex.io>
2021-01-28 22:31:21 +0100

    Test

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,6 +1,4 @@
 using HotChocolate;
-using HotChocolate.Execution;
-using HotChocolate.Execution.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
