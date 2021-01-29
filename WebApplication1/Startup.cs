2db8a1cc4d1a9c376978880262d16c09eade7ee4
Sebastian <sebastian@squidex.io>
2021-01-29 10:00:08 +0100

    Add query type schema

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,10 +1,13 @@
 using HotChocolate;
+using HotChocolate.Resolvers;
+using HotChocolate.Types;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace WebApplication1
 {
@@ -13,6 +16,80 @@ namespace WebApplication1
         public class Result
         {
             public string Value { get; set; }
+        }
+
+        public class ResultType : ObjectType<Result>
+        {
+            protected override void Configure(IObjectTypeDescriptor<Result> descriptor)
+            {
+                descriptor.Name("Result");
+
+                descriptor.Field(x => x.Value)
+                    .Type<NonNullType<StringType>>();
+            }
+        }
+
+        public class QueryFooType : ObjectType
+        {
+            protected override void Configure(IObjectTypeDescriptor descriptor)
+            {
+                descriptor.Field("both")
+                    .Resolve(new FieldResolverDelegate(x =>
+                    {
+                        return new ValueTask<object>(new List<Result>
+                        {
+                            new Result
+                            {
+                                Value = "foo_both"
+                            }
+                        });
+                    }))
+                    .Type<NonNullType<ListType<NonNullType<ResultType>>>>();
+
+                descriptor.Field("foo")
+                    .Resolve(new FieldResolverDelegate(x =>
+                    {
+                        return new ValueTask<object>(new List<Result>
+                        {
+                            new Result
+                            {
+                                Value = "foo_only"
+                            }
+                        });
+                    }))
+                    .Type<NonNullType<ListType<NonNullType<ResultType>>>>();
+            }
+        }
+        public class QueryBarType : ObjectType
+        {
+            protected override void Configure(IObjectTypeDescriptor descriptor)
+            {
+                descriptor.Field("both")
+                    .Resolve(new FieldResolverDelegate(x =>
+                    {
+                        return new ValueTask<object>(new List<Result>
+                        {
+                            new Result
+                            {
+                                Value = "bar_both"
+                            }
+                        });
+                    }))
+                    .Type<NonNullType<ListType<NonNullType<ResultType>>>>();
+
+                descriptor.Field("bar")
+                    .Resolve(new FieldResolverDelegate(x =>
+                    {
+                        return new ValueTask<object>(new List<Result>
+                        {
+                            new Result
+                            {
+                                Value = "bar_only"
+                            }
+                        });
+                    }))
+                    .Type<NonNullType<ListType<NonNullType<ResultType>>>>();
+            }
         }
 
         public class QueryFoo
@@ -39,6 +116,7 @@ namespace WebApplication1
                 };
             }
         }
+
         public class QueryBar
         {
             public List<Result> GetBoth()
@@ -81,6 +159,10 @@ namespace WebApplication1
                 .AddQueryType<QueryFoo>();
             services.AddGraphQLServer("myBar")
                 .AddQueryType<QueryBar>();
+            services.AddGraphQLServer("myFooType")
+                .AddQueryType<QueryFooType>();
+            services.AddGraphQLServer("myBarType")
+                .AddQueryType<QueryBarType>();
 #endif
         }
 
@@ -99,6 +181,8 @@ namespace WebApplication1
 
                 endpoints.MapGraphQL("/graphql/foo", "myFoo");
                 endpoints.MapGraphQL("/graphql/bar", "myBar");
+                endpoints.MapGraphQL("/graphql/fooType", "myFooType");
+                endpoints.MapGraphQL("/graphql/barType", "myBarType");
             });
         }
     }
